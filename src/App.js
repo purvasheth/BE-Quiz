804e831eea144d75f0ff6c3a0ac40e3db2d2f4ad
Purva Sheth <purvashet@gmail.com>
2020-10-16 00:32:19 +0530

    Modify and add enhancements - refresh of score and wrong questions on new file upload - score board in mobile shifted to top - radio buttons have unique name for all groups

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Papa from "papaparse";
 import Display from "./Display";
+import uuid from "react-uuid";
 import "./styles.css";
 
 class App extends React.Component {
@@ -10,9 +11,19 @@ class App extends React.Component {
       csvfile: undefined,
       data: undefined,
       error: 0,
+      wrong: 0,
+      correct: 0,
     };
     this.updateData = this.updateData.bind(this);
   }
+  incCorrect = () => {
+    const correct = this.state.correct + 1;
+    this.setState({ correct });
+  };
+  incWrong = () => {
+    const wrong = this.state.wrong + 1;
+    this.setState({ wrong });
+  };
 
   handleChange = (event) => {
     const file = event.target.files[0];
@@ -33,13 +44,16 @@ class App extends React.Component {
     } else {
       this.setState({ error: 1 });
     }
+    //set total correct and wrong here based on state.
   };
 
   updateData(result) {
     const initialData = result.data;
     const size = initialData.length;
-    const data = initialData.slice(0, size - 1);
-    this.setState({ data });
+    const data = initialData
+      .slice(0, size - 1)
+      .map((row) => ({ ...row, id: uuid() }));
+    this.setState({ data, correct: 0, wrong: 0 });
   }
 
   render() {
@@ -58,7 +72,7 @@ class App extends React.Component {
             onChange={this.handleChange}
           />
           <p />
-          <button onClick={this.importCSV} style={{cursor: "pointer"}}> Upload </button>
+          <button onClick={this.importCSV}> Upload </button>
 
           <p style={{ color: "red" }}>
             {this.state.error > 0 &&
@@ -70,6 +84,10 @@ class App extends React.Component {
         <Display
           data={this.state.data}
           filename={this.state.csvfile ? this.state.csvfile.name : ".csv"}
+          incCorrect={this.incCorrect}
+          incWrong={this.incWrong}
+          correct={this.state.correct}
+          wrong={this.state.wrong}
         />
         <div
           className="footer"
--- src/Display.js
+++ src/Display.js
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useImmer } from "use-immer";
 import { CSVLink } from "react-csv";
 import QuestionComponent from "./Question";
-import uuid from 'react-uuid';
 import "./styles.css";
 
-export default function Display({ data, filename }) {
-  const [cacheTotal, setCache] = useState(0);
-  const [wrong, setWrong] = useState(0);
-  const [correct, setCorrect] = useState(0);
+export default function Display({
+  data,
+  filename,
+  correct,
+  wrong,
+  incCorrect,
+  incWrong,
+}) {
   const [csvData, setCsvData] = useImmer([]);
   const total = data && data.length;
-
   useEffect(() => {
-    if(cacheTotal !== total){
-      setCorrect(0);
-      setWrong(0);
-      setCache(total);
+    if (data) {
+      setCsvData((draft) => []);
     }
-  },[cacheTotal, total])
-  
+  }, [data, setCsvData]);
+
   return (
     <div style={{ margin: "5%" }}>
       {data && (
@@ -29,7 +29,7 @@ export default function Display({ data, filename }) {
             Correct: {correct} / {correct + wrong}
           </p>
           <CSVLink filename={`wrong_${filename}`} data={csvData}>
-            <button style={{ backgroundColor: "tomato", cursor: "pointer" }}>
+            <button style={{ backgroundColor: "tomato" }}>
               Wrong Questions
             </button>
           </CSVLink>
@@ -37,18 +37,17 @@ export default function Display({ data, filename }) {
       )}
       {data &&
         data.map((row, index) => {
-          const { Question, Answer } = row;
+          const { Question, id } = row;
           return (
-            <div key={`${Answer}${index}`}>
+            <div key={id}>
               <p>
                 {index + 1}. {Question}
               </p>
               <QuestionComponent
                 row={row}
                 setCsvData={setCsvData}
-                setCorrect={setCorrect}
-                setWrong={setWrong}
-                inputName={uuid()}
+                incCorrect={incCorrect}
+                incWrong={incWrong}
               />
             </div>
           );
